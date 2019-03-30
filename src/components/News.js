b822e715479eb15706750ccba02237b368d5390a
bymartinez <byromartinez@gmail.com>
2019-03-30 10:44:01 -0600

    show news

--- src/components/News.js
+++ src/components/News.js
@@ -5,25 +5,46 @@ class News extends React.Component {
   state = {
     news: [],
     isLoading: false,
-  } 
+    texto: null,
+  }
 
-  buscarNoticias = (event) => {
-    const valor = event.target.value;
+  componentDidMount() {
+    this.setState({
+      texto: 'science',
+    });
+    this.buscarNoticias();
+  }
+
+  buscarNoticias = () => {
+    const valor = this.state.texto;
     axios
       .get(`https://newsapi.org/v2/everything?q=${valor}&apiKey=5219928e2e004879bdb867aa5b80aa07`)
       .then((response) => {
-        console.log(response.data);
-      });
+        this.setState({
+          news: response.data.articles,
+        });
+      }); 
+  }
+
+  cambiarValor = (event) => {
+    const valor = event.target.value;
+    this.setState({
+      texto: valor,
+    });
   }
 
   render() {
     return (
       <div>
         <input 
-          onChange={this.buscarNoticias}
+          onChange={this.cambiarValor}
           type="text"
           className='form-control' 
           placeholder='Buscar noticias...' />
+        <button onClick={this.buscarNoticias}>Buscar</button>
+
+        
+        {this.state.news.map((noticia) => <h1>{noticia.title}</h1>)}
       </div>
     );
   }
