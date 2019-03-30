2366a0f4e11fa5c8c19a7225d13fd601d0456811
bymartinez <byromartinez@gmail.com>
2019-03-30 10:33:13 -0600

    axios get request login

--- src/components/News.js
+++ src/components/News.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 
 class News extends React.Component {
   state = {
     news: [],
     isLoading: false,
-  }
+  } 
 
   buscarNoticias = (event) => {
     const valor = event.target.value;
-    console.log(valor);
+    axios
+      .get(`https://newsapi.org/v2/everything?q=${valor}&apiKey=5219928e2e004879bdb867aa5b80aa07`)
+      .then((response) => {
+        console.log(response.data);
+      });
   }
 
   render() {
